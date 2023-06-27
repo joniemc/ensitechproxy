64be0a7cc48aa2e72168ee7ab7b15074abe0acb8
jonie_mirc <jonie.mirc@gmail.com>
2023-06-27 14:18:06 -0600

    Proxy Module First Version, started project only.

---
+++ proxymodule/src/test/java/mx/com/ensitech/proxymodule/ProxyModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.ensitech.proxymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
